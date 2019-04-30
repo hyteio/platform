6f42cc5326924788860c15dc80b49391f5aceb14
Matt Pavlovich <matt@hyte.io>
2019-04-30 00:15:03 -0500

    [HY-81] First pass at updating hyte-runtime to Apache Karaf v4.2.5

--- hyte-runtime/src/main/resources/karaf-win64/bin/setenv.bat
+++ hyte-runtime/src/main/resources/karaf-win64/bin/setenv.bat
@@ -1,21 +1,4 @@
 @echo off
-rem
-rem
-rem    Licensed to the Apache Software Foundation (ASF) under one or more
-rem    contributor license agreements.  See the NOTICE file distributed with
-rem    this work for additional information regarding copyright ownership.
-rem    The ASF licenses this file to You under the Apache License, Version 2.0
-rem    (the "License"); you may not use this file except in compliance with
-rem    the License.  You may obtain a copy of the License at
-rem
-rem       http://www.apache.org/licenses/LICENSE-2.0
-rem
-rem    Unless required by applicable law or agreed to in writing, software
-rem    distributed under the License is distributed on an "AS IS" BASIS,
-rem    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-rem    See the License for the specific language governing permissions and
-rem    limitations under the License.
-rem
 
 rem Get Current DateTime stamp
 for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined NOW set NOW=%%x
@@ -45,16 +28,13 @@ rem karaf script afterwards.
 rem
 
 rem Begin HYTE
-set JAVA_CMS_GC_OPTS=-XX:ParallelGCThreads=2 -XX:+UseConcMarkSweepGC -XX:+UseParNewGC
-set JAVA_CMS_GC_ADV_OPTS=-XX:+CMSConcurrentMTEnabled -XX:+CMSClassUnloadingEnabled -XX:+CMSParallelRemarkEnabled -XX:+ExplicitGCInvokesConcurrent -XX:+ExplicitGCInvokesConcurrentAndUnloadsClasses -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 -XX:+ScavengeBeforeFullGC -XX:+CMSScavengeBeforeRemark
 set JAVA_G1_GC_OPTS=-XX:+UseG1GC -XX:+UseStringDeduplication -XX:MaxGCPauseMillis=800 
 set JAVA_GC_LOG_OPTS=-verbose:gc -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -XX:+PrintGCCause -Xloggc:data\log\gc-%KARAF_STARTDATE%.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=16 -XX:GCLogFileSize=16M 
 set JAVA_HEAPDUMP_OPTS=-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=data\log\%KARAF_STARTDATE%.hprof
-set JAVA_MISC_OPTS=-server
 set JAVA_NET_OPTS=-Dsun.net.inetaddr.ttl=60 -Dsun.net.client.defaultConnectTimeout=300000 -Dsun.net.client.defaultReadTimeout=600000
 rem END HYTE
 
-set EXTRA_JAVA_OPTS=%JAVA_G1_GC_OPTS% %JAVA_GC_LOG_OPTS% %JAVA_HEAPDUMP_OPTS% %JAVA_MISC_OPTS% %JAVA_NET_OPTS%
+set EXTRA_JAVA_OPTS=%JAVA_G1_GC_OPTS% %JAVA_GC_LOG_OPTS% %JAVA_HEAPDUMP_OPTS% %JAVA_NET_OPTS%
 rem SET KARAF_HOME
 rem SET KARAF_DATA
 rem SET KARAF_BASE
