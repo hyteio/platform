7a109e79a54cc432818fa516783cbf174beb2737
Matt Pavlovich <matt@hyte.io>
2021-05-14 16:14:17 -0500

    Hp 191 (#192)
    * [#180] Upgrade to karaf 4.2.11
    
     - Leverage the Karaf BOM
    
    * [#191] Move ./start and ./karaf options to fix large heap and ./client issues

--- hyte-runtime/src/main/resources/karaf-win64/bin/setenv.bat
+++ hyte-runtime/src/main/resources/karaf-win64/bin/setenv.bat
@@ -18,8 +18,8 @@ IF "%KARAF_SCRIPT%" == "client.bat" OR "%KARAF_SCRIPT%" == "status.bat" OR "%KAR
 ) ELSE (
     set JAVA_MIN_MEM=512M
     set JAVA_MAX_MEM=2G
-    set JAVA_GC_LOG_OPTS=-Xlog:gc*:file=data\log\gc-%p-%t.log:time,hostname,uptime,pid,tags,level:filecount=16,filesize=16M
-    set JAVA_SP_LOG_OPTS=-Xlog:safepoint*:file=data\log\safepoints-%p-%t.log:time,hostname,uptime,pid,tags,level:filecount=16,filesize=16m
+    set JAVA_GC_LOG_OPTS=-Xlog:gc*:file=data\log\gc.log:time,hostname,uptime,pid,tags,level:filecount=16,filesize=16M
+    set JAVA_SP_LOG_OPTS=-Xlog:safepoint*:file=data\log\safepoints.log:time,hostname,uptime,pid,tags,level:filecount=16,filesize=16m
 )
 
 rem
@@ -32,7 +32,7 @@ rem
 rem Begin HYTE
 set JAVA_G1_GC_OPTS=-XX:+UseG1GC -XX:+UseStringDeduplication -XX:MaxGCPauseMillis=800 
 set JAVA_HEAPDUMP_OPTS=-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=data\log\jvm-heapdump-%KARAF_STARTDATE%.hprof
-set JAVA_NET_OPTS=-Dsun.net.inetaddr.ttl=60 -Dsun.net.client.defaultConnectTimeout=300000 -Dsun.net.client.defaultReadTimeout=600000
+set JAVA_NET_OPTS=-Dnetworkaddress.cache.ttl=60 -Dsun.net.client.defaultConnectTimeout=30000 -Dsun.net.client.defaultReadTimeout=300000
 rem END HYTE
 
 set EXTRA_JAVA_OPTS=%JAVA_G1_GC_OPTS% %JAVA_GC_LOG_OPTS% %JAVA_SP_LOG_OPTS% %JAVA_HEAPDUMP_OPTS% %JAVA_NET_OPTS%
